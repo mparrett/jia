c35668bff2cff874c2d3d8aaffd9e110fafac079
Matt Parrett <matt@plainvanillagames.com>
2016-04-17 09:46:16 -0700

    Fix portability issue in run.sh, minor changes

--- install-venv-2.7.sh
+++ install-venv-2.7.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SRC_PY_PATH=/usr/local/python-2.7/bin
+# Expects python 2.7
 
-echo "Reinstalling virtualenv..."
-rm -rf ENV-2.7
-$SRC_PY_PATH/virtualenv ENV-2.7
-source ENV-2.7/bin/activate || exit 1
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VENV_DIR=./venv
+
+echo "Installing virtualenv..."
+rm -rf $VENV_DIR
+virtualenv $VENV_DIR
+
+source $VENV_DIR/bin/activate || (echo "Could not prepare virtualenv. How's your python install?"; exit 1)
 
 pip install -r $SCRIPT_DIR/requirements-2.7.txt
