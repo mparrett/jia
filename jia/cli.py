9817b4b8f3cffa29ba5d817822cfbfdf0ba1097c
Matt Parrett <matt@plainvanillagames.com>
2016-04-17 12:58:52 -0700

    Improve argument description, minor fixes

--- jia/cli.py
+++ jia/cli.py
@@ -3,19 +3,19 @@
 """jia - Simple Shell Script Provisioner
 
 Usage:
-    jia.py configure (--def=<def>) (--host=<host>) [--port=<port>] [--user=<user>] [--key=<key>] [--pass=<pass>] [--vars=<vars>] [--match=<match>] [-v]
+    jia.py configure <server-def> [options] 
     jia.py -h|--help
 
 Options:
     -h --help                   Show this screen.
     -v --verbose                Be more verbose.
-    --port=<port>               SSH Port [default: 22].
-    --user=<user>               SSH User [default: root].
-    --key=<key>                 SSH Key.
-    --host=<host>               Server host.
-    --def=<def>                 Server definition.
-    --vars=<vars>               YML file containing template variables.
-    --match=<match>             Only run scripts that match specified pattern.
+    --host=<host>               Server host [default: localhost].
+    -p --port=<port>            SSH Port [default: 22].
+    -u --user=<user>            SSH User.
+    --pass=<pass>               SSH Password.
+    -k --key=<key>              SSH Key.
+    --vars=<vars>               YAML file containing template variables.
+    --match=<match>             Only run scripts matching specified pattern.
 """
 
 import os
@@ -24,6 +24,8 @@ import yaml
 import signal
 import time
 import re
+
+from jia import __version__
 
 from paramiko import SSHClient, AutoAddPolicy, Channel, Transport
 from textcolor import textcolor, RED, GREEN, YELLOW
@@ -39,7 +41,7 @@ ssh.set_missing_host_key_policy(AutoAddPolicy())
 trans = None
 chan = None
 
-args = docopt(__doc__, version='0.9')
+args = docopt(__doc__, version=__version__)
 
 
 def sigint_handler(signum, frame):
@@ -96,39 +98,49 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, sigint_handler)
 
     try:
-        if args['--key']:
-            key = os.path.expanduser(args['--key'])
-            ssh.connect(args['--host'], int(args['--port']), args['--user'], key_filename=key, timeout=5)
-        else:
-            ssh.connect(args['--host'], int(args['--port']), args['--user'], args['--pass'], timeout=5)
+        # Priority: CLI, environment, Vagrant
+        key_file = args['--key'] or os.getenv('SSH_PRIVATE_KEY') or '~/.vagrant.d/insecure_private_key'
+
+        if args['--verbose']:
+            print "Using key file: {}".format(key_file)
+            print "Connecting to {}:{}".format(args['--host'], args['--port'])
+
+        key_file = os.path.expanduser(key_file)
+        if not os.path.isfile(key_file):
+            print "Error: Private key file does not exist: {}".format(key_file)
+            sys.exit(1)
+
+        ssh.connect(args['--host'], int(args['--port']), args['--user'], key_filename=key_file, timeout=5)
+
     except Exception as e:
-        print "Could not establish a connection to {}:{}".format(args['--host'], args['--port'])
+        print "Error: Couldn't establish connection to {}:{}".format(args['--host'], args['--port'])
         print e
         sys.exit(1)
 
     trans = ssh.get_transport()
     chan = trans.open_channel('session')
 
+    sys.exit(0)
+
     try:
-        definition = open(os.getcwd() + '/actions/' + args['--def']).read()
+        definition = open(os.getcwd() + '/actions/' + args['server-def']).read()
     except:
-        print "Definition not found: " + args['--def']
+        print "Definition not found: " + args['server-def']
         sys.exit(1)
 
     scp = SCPClient(ssh.get_transport())
 
     server_def = yaml.load(definition)
 
-    # Let's do some basic checks on the server definition
+    # Do some basic checks on the server definition
 
     if not 'scripts' in server_def:
-        print "Server definition missing 'scripts'. Won't do anything!"
-        sys.exit(1)
+        print "WARNING: Server definition missing 'scripts'. Won't run anything!"
 
     if 'files' in server_def and server_def['files'] is not None:
         for f in (server_def['files'] + server_def['scripts']):
             if not os.path.isfile(os.getcwd() + '/' + f):
-                print "Specified file or script not found: " + os.getcwd() + '/' + f
+                print "Error: File or script not found: " + os.getcwd() + '/' + f
                 sys.exit(1)
 
     # Now the fun part!
@@ -139,7 +151,7 @@ if __name__ == '__main__':
         try:
             template_vars = yaml.load(open(os.getcwd() + '/vars/' + args['--vars']).read())
         except:
-            print 'Specified variables file not found: ' + args['--vars']
+            print 'Error: Variables file not found: ' + args['--vars']
             sys.exit(1)
 
     #pp(template_vars)
@@ -153,12 +165,14 @@ if __name__ == '__main__':
                 copy_files.append(f)
                 continue
 
-            #print "Rendering template {} {} {}".format(f, os.getcwd() + '/' + f, f.replace('.j2', ''))
-            print "Rendering template {}".format(f)
+            if args['--verbose--']:
+                #print "Rendering template {} {} {}".format(f, os.getcwd() + '/' + f, f.replace('.j2', ''))
+                print "Rendering template {}".format(f)
 
             t = Template(open(os.getcwd() + '/' + f).read())
             if not os.path.exists(os.path.dirname('/tmp/jia-py/' + f.replace('.j2', ''))):
                 os.makedirs(os.path.dirname('/tmp/jia-py/' + f.replace('.j2', '')))
+
             open('/tmp/jia-py/' + f.replace('.j2', ''), 'w').write(t.render(template_vars))
 
             copy_files.append(f.replace('.j2', ''))
@@ -172,14 +186,14 @@ if __name__ == '__main__':
     ret = create_local_archive(filename, server_def['scripts'] + copy_files)
 
     if ret != 0:
-        print "Could not create local archive"
+        print "Error: Could not create local archive"
         sys.exit(ret)
 
     ssh_command('mkdir /tmp/jia-py >/dev/null 2>&1')
     scp.put('/tmp/jia-py/' + filename, '/tmp/jia-py/' + filename)
     extract_remote_archive('/tmp/jia-py/' + filename)
 
-    # NOW RUN THE SCRIPTS!
+    # Now run the scripts
 
     for script in server_def['scripts']:
         if not args['--match'] or re.search(args['--match'], script) is not None:
